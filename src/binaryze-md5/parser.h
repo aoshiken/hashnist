f52313445a68ad11d41e501465417b200e310a34
Alfredo <aandreswork@hotmail.com>
2021-06-06 01:02:21 +0200

    Initial support for MD5 hashes

---
+++ src/binaryze-md5/parser.h
@@ -0,0 +1,8 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include "context.h"
+
+bool parse_options( int argc, char **argv, CONTEXT *ctx );
+
+#endif
---
+++ src/common/md5.h
@@ -0,0 +1,44 @@
+#ifndef __MD5_H__
+#define __MD5_H__
+
+#include <inttypes.h>
+#include <regex.h>
+
+typedef union _MD5 {
+
+    __uint128_t num128 ;
+
+    struct {
+        uint64_t first ;
+        uint64_t second ;
+    } num64 ;
+
+} MD5 ;
+
+
+typedef struct _MD5_CONTEXT {
+    unsigned char *buffer ; // Read buffer
+    MD5 *md5_first ;    // First MD5 in buffer
+    MD5 *md5_last ;     // Last MD5 in buffer
+    regex_t preg ;      // Regex handle
+} MD5_CONTEXT ;
+
+enum md5_search_ret
+{
+    MD5_SEARCH_FOUND     = 0,
+    MD5_SEARCH_NOT_FOUND = 1,
+    MD5_SEARCH_ERROR     = 2
+};
+
+MD5_CONTEXT *md5_init_ctx( const char *file_path );
+void md5_fini_ctx( MD5_CONTEXT *md5_ctx );
+md5_search_ret md5_search( MD5_CONTEXT *md5_ctx, char *hex_str ); 
+char *md5_to_hex( MD5 *md5 );
+MD5 *md5_from_hex( char *hex_str );
+bool md5_from_hex_allocated( char *hex_str, MD5 *ret_md5 );
+bool md5_less_or_equal( MD5 *md5_left, MD5 *md5_right );
+bool md5_great( MD5 *md5_left, MD5 *md5_right );
+bool md5_great_or_equal( MD5 *md5_left, MD5 *md5_right );
+bool md5_not_equal( MD5 *md5_left, MD5 *md5_right );
+
+#endif
