fa4ad81d328d9602d7bd524468c51c31af88aaff
Alfredo <aoshiken@users.noreply.github.com>
2021-08-04 14:29:06 +0200

    Fix parameter name in tasks.py

--- web/tasks.py
+++ web/tasks.py
@@ -9,11 +9,11 @@ from hashnist.analysis import analyze as hashnist_analyze
 
 def analyze( hash_list ):
 
-    result_obj = hashnist_analyze( hash_list   = hash_list,
-                                   server_name = settings.HOSTNAME,
-                                   server_port = settings.PORT,
-                                   group_size  = settings.GROUP_SIZE,
-                                   timeout     = settings.SOCKET_TIMEOUT )
+    result_obj = hashnist_analyze( hash_list    = hash_list,
+                                   server_name  = settings.HOSTNAME,
+                                   server_port  = settings.PORT,
+                                   group_size   = settings.GROUP_SIZE,
+                                   sock_timeout = settings.SOCKET_TIMEOUT )
 
     ret_dict = { 'status' : 'FINISHED', 'results' : result_obj.to_dict() }
 
